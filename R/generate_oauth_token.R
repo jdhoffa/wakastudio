fdb106133817658e8e57bdcaf847cdeacaed66cb
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-02-11 22:50:33 +0100

    Begin migration to httr2 oauth flow

--- R/generate_oauth_token.R
+++ R/generate_oauth_token.R
@@ -1,24 +1,35 @@
 #' Generate a token for the WakaTime API
 #'
-#' @param app_id The app id for the WakaTime API
-#' @param app_secret The app secret for the WakaTime API
+#' @param client_id The app id for the WakaTime API
+#' @param client_secret The app secret for the WakaTime API
 #'
 #' @return An OAuth token
 #'
 #' @examples
-generate_oauth_token <- function(app_id = Sys.getenv("WAKATIME_ID"),
-                      app_secret = Sys.getenv("WAKATIME_SECRET")) {
-  httr::oauth2.0_token(
-    endpoint = httr::oauth_endpoint(
-      access = "https://wakatime.com/oauth/token",
-      authorize = "https://wakatime.com/oauth/authorize",
-      base_url = "https://api.wakatime.com/api/v1"
-    ),
-    app = httr::oauth_app(
-      appname = "wakastudio",
-      key = app_id,
-      secret = app_secret
-    ),
-    scope = "read_stats"
+generate_oauth_token <- function(client_id = Sys.getenv("WAKATIME_ID"),
+                                 client_secret = Sys.getenv("WAKATIME_SECRET")) {
+  httr2::oauth_flow_client_credentials(
+    client = wakatime_client(client_id, client_secret),
+    scope = NULL,
+    token_params = list(
+      client_id = client_id,
+      client_secret = client_secret,
+      redirect_uri = "https://wakatime.com/oauth/authorize",
+      response_type = "token"
+    )
+  )
+
+  httr2::oauth_token(
+# INSERT TOKEN HERE
+    )
+
+}
+
+wakatime_client <- function(client_id, client_secret) {
+  httr2::oauth_client(
+    name = "wakatime",
+    id = client_id,
+    secret = client_secret,
+    token_url = "https://wakatime.com/oauth/token"
   )
 }
