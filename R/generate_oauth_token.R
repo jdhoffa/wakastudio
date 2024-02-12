edc324a50609b77f85e0c95ee403c1c134e06789
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-02-12 08:46:28 +0100

    Fix OAuth gen using test redirect_uri

--- R/generate_oauth_token.R
+++ R/generate_oauth_token.R
@@ -1,35 +1,44 @@
 #' Generate a token for the WakaTime API
 #'
-#' @param client_id The app id for the WakaTime API
-#' @param client_secret The app secret for the WakaTime API
+#' @param client_id The app id for the WakaTime API. Defaults to the WAKATIME_ID
+#' environment variable.
+#' @param client_secret The app secret for the WakaTime API. Defaults to the
+#' WAKATIME_SECRET environment variable.
 #'
 #' @return An OAuth token
 #'
 #' @examples
+#' \dontrun{
+#' token <- generate_oauth_token()
+#'
+#' req <- request("https://api.wakatime.com/api/v1/users/current")
+#'
+#' req |>
+#'   req_oauth_auth_code(
+#'     client = wakatime_client(client_id, client_secret),
+#'     auth_url = "https://wakatime.com/oauth/authorize",
+#'     scope = "write_heartbeats",
+#'     redirect_uri = "https://wakatime.com/oauth/test"
+#'     ) |>
+#'   req_perform() |>
+#'   resp_body_json() |>
+#'   str()
+#' }
 generate_oauth_token <- function(client_id = Sys.getenv("WAKATIME_ID"),
                                  client_secret = Sys.getenv("WAKATIME_SECRET")) {
-  httr2::oauth_flow_client_credentials(
+  httr2::oauth_flow_auth_code(
     client = wakatime_client(client_id, client_secret),
+    auth_url = "https://wakatime.com/oauth/authorize",
     scope = NULL,
-    token_params = list(
-      client_id = client_id,
-      client_secret = client_secret,
-      redirect_uri = "https://wakatime.com/oauth/authorize",
-      response_type = "token"
-    )
+    redirect_uri = "https://wakatime.com/oauth/test"
   )
-
-  httr2::oauth_token(
-# INSERT TOKEN HERE
-    )
-
 }
 
 wakatime_client <- function(client_id, client_secret) {
   httr2::oauth_client(
-    name = "wakatime",
     id = client_id,
+    token_url = "https://wakatime.com/oauth/token",
     secret = client_secret,
-    token_url = "https://wakatime.com/oauth/token"
+    name = "wakatime"
   )
 }
